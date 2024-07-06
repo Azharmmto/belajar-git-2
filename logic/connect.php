977eda191d813c892b2c88495e63f9753e69fffa
Azharmmto <azharmamonto7@gmail.com>
2024-07-06 23:01:10 +0800

    Initial commit

---
+++ logic/connect.php
@@ -0,0 +1,11 @@
+<?php 
+
+  // connect database
+  $hostName = "localhost";
+  $userName = "root";
+  $password = "";
+  $dbName = "belajar_git";
+
+  mysqli_connect($hostName, $userName, $password, $dbName);
+
+?>
