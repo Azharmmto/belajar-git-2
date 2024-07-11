7e0ce6caefb5b6ad1f25f287e2815b889898a571
Azharmmto <azharmamonto7@gmail.com>
2024-07-11 08:55:52 +0800

    Menambahkan table dan stylenya

--- index.php
+++ index.php
@@ -26,5 +26,31 @@
     </div>
   </nav>
 
+  <main>
+    <header>
+      <h2>Daftar Barang</h2>
+    </header>
+
+    <table border="1">
+      <thead>
+        <tr>
+          <th>Nama</th>
+          <th>Kategori</th>
+          <th>Deskripsi</th>
+          <th>Harga</th>
+        </tr>
+      </thead>
+        <tr>
+          <td>Pemarograam Web Dasar</td>
+          <td>Buku Bacaan</td>
+          <td>Buku dengan judul Pemrograman web dasar, ditulis oleh Alan S.Kom penerbit informatika</td>
+          <td>Rp. 63.000</td>
+        </tr>
+      <tbody>
+        
+      </tbody>
+    </table>
+  </main>
+
 </body>
 </html>
